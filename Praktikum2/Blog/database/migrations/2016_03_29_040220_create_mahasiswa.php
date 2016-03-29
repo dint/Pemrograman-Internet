ef40eda9d53521ed960951fc0e5a762252fcfbba
dint <fahrudin1705@gmail.com>
2016-03-29 11:30:11 +0700

    praktikum2

---
+++ Praktikum2/Blog/database/migrations/2016_03_29_040220_create_mahasiswa.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMahasiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim');
+	    $table->string('nama_mhs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('mahasiswa');
+    }
+}
